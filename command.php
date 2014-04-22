88676655cfe8060a9bca0239bd68cef1eb2f975d
Piotr Kierzniewski <kierzniak@gmail.com>
2014-04-23 01:33:36 +0200

    Use color from config.yaml

--- command.php
+++ command.php
@@ -18,12 +18,11 @@ if ( is_readable( $config_path ) ){
     list( $config, $extra_config ) = $configurator->to_array();
 }
 
-if ( isset($config['color']) && $config['color'] === 'auto' ) {
-    $colorize = !\cli\Shell::isPiped();
+if ( 'auto' === $config['color'] ) {
+  $colorize = !\cli\Shell::isPiped();
 } else {
-    $colorize = true;
+  $colorize = $config['color'];
 }
-
 
 if ( isset($config['quiet']) && $config['quiet'] )
     $logger = new \WP_CLI\Loggers\Quiet;
